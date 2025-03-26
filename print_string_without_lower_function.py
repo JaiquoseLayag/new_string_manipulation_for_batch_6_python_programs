9087eb4dd628e4dc6b55109ec0d4ca2419693cce
Jacuzzi <jheklayag@gmail.com>
2025-03-26 20:14:45 +0800

    A loop is used to check every uppercase letter

--- print_string_without_lower_function.py
+++ print_string_without_lower_function.py
@@ -1,5 +1,9 @@
 # Allow the user to input a string
 string = input("Enter a string: ")
 # Use a loop and check for every uppercase letter 
+lowercase_format = ""
+
+for characters in string:
+    if 'A' <= character <= 'Z':
 # Add 32 to every ASCII code of each uppercase letter
 # Print result
