0585add339f0f09f7153371c5fe926eccc8dc37e
Jacuzzi <jheklayag@gmail.com>
2025-03-26 18:46:29 +0800

    User input for the string and prefix used in the string has been added

--- print_string_without_prefix_without_removeprefix.py
+++ print_string_without_prefix_without_removeprefix.py
@@ -1,4 +1,6 @@
 # Allow the user to input both a string and the prefix of the string
+string = input("Enter a string: ")
+prefix = input("Enter the prefix: ")
 # Use startswith() to check if the string strats with the prefix
 # Slice the string starting from the index without the prefix
 # Print the string without the prefix
