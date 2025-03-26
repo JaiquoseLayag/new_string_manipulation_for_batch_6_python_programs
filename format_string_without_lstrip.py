02fee9473c712998140b2ff6fd540a9d643f91db
Jacuzzi <jheklayag@gmail.com>
2025-03-26 18:20:05 +0800

    User input for string has been added

--- format_string_without_lstrip.py
+++ format_string_without_lstrip.py
@@ -1,4 +1,5 @@
 # Allow the user to input a string with leading spaces
+string = input("Enter a string: ")
 # Use a for loop to iterate through each charcter and check if it's a space
-# Print the string once the first non-spacce character is found
+# Print the string once the first non-space character is found
 # Break the loop
