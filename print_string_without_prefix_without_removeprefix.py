ce330a4e5d9d0457413486d33a5bad7124584a8b
Jacuzzi <jheklayag@gmail.com>
2025-03-26 18:45:10 +0800

    Pseudocode

---
+++ print_string_without_prefix_without_removeprefix.py
@@ -0,0 +1,4 @@
+# Allow the user to input both a string and the prefix of the string
+# Use startswith() to check if the string strats with the prefix
+# Slice the string starting from the index without the prefix
+# Print the string without the prefix
