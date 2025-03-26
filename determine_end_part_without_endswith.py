bf9ed2b6c008d64338badcd3511f4e1ee12cc1ff
Jacuzzi <jheklayag@gmail.com>
2025-03-26 20:50:01 +0800

    len() has been used to check length of suffix and negative index to pull out the end part

--- determine_end_part_without_endswith.py
+++ determine_end_part_without_endswith.py
@@ -1,5 +1,7 @@
 # Allow the user to input a string and the suffix
 string = input("Enter a string: ")
 suffix = input("Enter the suffix: ")
-# Get the length of the suffix and the end part of the string
+# Get the length of the suffix and slice the main string
+length = len(suffix)
+end_part = string[-length]
 # Print True or False if the end part and suffix are the same or not
