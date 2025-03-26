58113812a78f7947c4fdc04a68837bca295d131b
Jacuzzi <jheklayag@gmail.com>
2025-03-26 20:29:25 +0800

    Checks to see if characters are uppercase with its ASCII value

--- determine_if_uppercase_without_isupper.py
+++ determine_if_uppercase_without_isupper.py
@@ -2,3 +2,8 @@
 string = input("Enter a string: ")
 # Use a loop and check the ASCII values of each letter
 # Return false if the value is greater than 90
+for characters in string:
+    if not ('A' <= characters <= 'Z'):
+        print("False")
+    else:
+        print("True")
