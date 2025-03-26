b6b08208b92f88f15066613c04932354884b3cee
Jacuzzi <jheklayag@gmail.com>
2025-03-26 20:30:40 +0800

    Fixed the pseudocode

--- determine_if_uppercase_without_isupper.py
+++ determine_if_uppercase_without_isupper.py
@@ -1,7 +1,7 @@
 # Allow the user to input a string
 string = input("Enter a string: ")
 # Use a loop and check the ASCII values of each letter
-# Return false if the value is greater than 90
+# Return false if character is within A-Z
 for characters in string:
     if not ('A' <= characters <= 'Z'):
         print("False")
