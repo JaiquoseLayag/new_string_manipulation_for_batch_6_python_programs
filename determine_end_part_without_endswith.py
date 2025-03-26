87ce3eb9c4433a406febb2207941a104499c839d
Jacuzzi <jheklayag@gmail.com>
2025-03-26 20:47:00 +0800

    Pseudocode

---
+++ determine_end_part_without_endswith.py
@@ -0,0 +1,3 @@
+# Allow the user to input a string and the suffix
+# Get the length of the suffix and the end part of the string
+# Print True or False if the end part and suffix are the same or not
