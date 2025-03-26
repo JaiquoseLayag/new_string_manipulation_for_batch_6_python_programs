6e00e28f8ba43cae9949bbba782903bc6d3e5784
Jacuzzi <jheklayag@gmail.com>
2025-03-26 18:21:25 +0800

    Loop checks every space in the string

--- format_string_without_lstrip.py
+++ format_string_without_lstrip.py
@@ -1,5 +1,7 @@
 # Allow the user to input a string with leading spaces
 string = input("Enter a string: ")
 # Use a for loop to iterate through each charcter and check if it's a space
+for i in range(len(string)):
+    if string[i] != " ":
 # Print the string once the first non-space character is found
 # Break the loop
