c9c793ba5efad6f31c34e742b69d8d1797a4ac39
Jacuzzi <jheklayag@gmail.com>
2025-03-26 18:16:19 +0800

    Pseudocode

---
+++ format_string_without_lstrip.py
@@ -0,0 +1,4 @@
+# Allow the user to input a string with leading spaces
+# Use a for loop to iterate through each charcter and check if it's a space
+# Print the string once the first non-spacce character is found
+# Break the loop
