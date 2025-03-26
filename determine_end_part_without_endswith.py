e40476ad4913118cac87a4b7b257805e54d248d5
Jacuzzi <jheklayag@gmail.com>
2025-03-26 20:48:02 +0800

    User input for string and suffix has been added

--- determine_end_part_without_endswith.py
+++ determine_end_part_without_endswith.py
@@ -1,3 +1,5 @@
 # Allow the user to input a string and the suffix
+string = input("Enter a string: ")
+suffix = input("Enter the suffix: ")
 # Get the length of the suffix and the end part of the string
 # Print True or False if the end part and suffix are the same or not
