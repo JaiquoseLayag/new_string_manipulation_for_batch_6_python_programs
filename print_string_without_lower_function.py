b8fcaee7edd4714900882567ded43bea3aeb7faf
Jacuzzi <jheklayag@gmail.com>
2025-03-26 20:10:47 +0800

    User input for string has been added

--- print_string_without_lower_function.py
+++ print_string_without_lower_function.py
@@ -1,4 +1,5 @@
 # Allow the user to input a string
+string = input("Enter a string: ")
 # Use a loop and check for every uppercase letter 
 # Add 32 to every ASCII code of each uppercase letter
 # Print result
