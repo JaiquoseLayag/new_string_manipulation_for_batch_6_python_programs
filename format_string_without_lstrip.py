af3d2ce8fe4d8ca0ca6db5310b6b29e3a77fd3eb
Jacuzzi <jheklayag@gmail.com>
2025-03-26 18:23:35 +0800

    The list is sliced from the start of a non-space character and result has been printed

--- format_string_without_lstrip.py
+++ format_string_without_lstrip.py
@@ -1,7 +1,11 @@
 # Allow the user to input a string with leading spaces
 string = input("Enter a string: ")
 # Use a for loop to iterate through each charcter and check if it's a space
-for i in range(len(string)):
-    if string[i] != " ":
+for word in range(len(string)):
+    if string[word] != " ":
 # Print the string once the first non-space character is found
 # Break the loop
+    result = string[word:]
+    break
+
+print(result)
