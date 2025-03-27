3b173bf113ba62ce83d119d02607bc4206e3eab7
Jacuzzi <jheklayag@gmail.com>
2025-03-27 13:50:47 +0800

    Pseudocode

---
+++ fortmat_string_without_center.py
@@ -0,0 +1,4 @@
+# Allow the user to input string and desired length
+# Calculate how many spaces to be added to the left and right of the string
+# Manually add spaces to the left and right of the string
+# Print result
