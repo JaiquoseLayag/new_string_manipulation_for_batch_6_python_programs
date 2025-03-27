07464e505de11e8818a16fe73a103ef3ffbf50a2
Jacuzzi <jheklayag@gmail.com>
2025-03-27 14:04:15 +0800

    User input for string has been added

--- format_string_without_swapcase.py
+++ format_string_without_swapcase.py
@@ -1,4 +1,5 @@
 # Allow the user to input a string
+string = input("Enter a string: ")
 # Use a loop and check for every uppercase and/or lowercase letter
 # Add or subtract 32 to every ASCII code of each uppercase and/or lowercase letter
 # Print result
