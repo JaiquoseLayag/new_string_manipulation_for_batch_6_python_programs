0b3db0c29f614a9eb4e0d6b377b9022826cbabcd
Jacuzzi <jheklayag@gmail.com>
2025-03-27 14:14:46 +0800

    .upper() and .lower() have been used on the first and remainingletters respectively

--- format_string_without_capitalize.py
+++ format_string_without_capitalize.py
@@ -1,4 +1,8 @@
 # Allow the user to input a string
 string = input("Enter a string: ")
 # Use upper() on the first letter and lower() on the remaining letters
+first_letter = string[0].upper()
+remaining = string[1:].lower()
+
+new_string = first_letter + remaining
 # Print result
