24dbc88616377081673bfbf3517934056125b4df
Jacuzzi <jheklayag@gmail.com>
2025-03-27 14:05:41 +0800

    32 has been added or subtracted to the ASCII value of each letter depending on the casing

--- format_string_without_swapcase.py
+++ format_string_without_swapcase.py
@@ -2,4 +2,13 @@
 string = input("Enter a string: ")
 # Use a loop and check for every uppercase and/or lowercase letter
 # Add or subtract 32 to every ASCII code of each uppercase and/or lowercase letter
+new_format = ""
+
+for character in string:
+    if 'A' <= character <= 'Z':
+        new_format += chr(ord(character) + 32)
+    elif 'a' <= character <= 'z':
+        new_format += chr(ord(character) - 32)
+    else:
+        new_format += character
 # Print result
