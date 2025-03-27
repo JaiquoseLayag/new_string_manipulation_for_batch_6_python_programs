4768aac7c22c0a89698f461cbe0913049dd0c412
Jacuzzi <jheklayag@gmail.com>
2025-03-27 14:28:45 +0800

    User input for string has been added

--- format_string_without_title.py
+++ format_string_without_title.py
@@ -1,4 +1,5 @@
 # Allow the user to input a string
+string = input("Enter a string: ")
 # Use split() to separate each word
 # Use upper() and lower() on the first and remaining letters
 # Use join() to add all new words together
