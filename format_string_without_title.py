9eecfb469d0a95f2ca8ecee73019a01cce623af4
Jacuzzi <jheklayag@gmail.com>
2025-03-27 14:30:06 +0800

    Words have been separated and applied upper() and lower() to the first and remaining letters

--- format_string_without_title.py
+++ format_string_without_title.py
@@ -2,5 +2,13 @@
 string = input("Enter a string: ")
 # Use split() to separate each word
 # Use upper() and lower() on the first and remaining letters
+split = string.split()
+words = []
+
+for word in split:
+    first_letter = word[0].upper()
+    remaining = word[1:].lower()
+    new_words = first_letter + remaining
+    words.append(new_words)
 # Use join() to add all new words together
 # Print result
