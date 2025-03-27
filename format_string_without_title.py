676c34d5d19e91aeb6eb299f04d1c47ea1e7f07e
Jacuzzi <jheklayag@gmail.com>
2025-03-27 14:28:00 +0800

    Pseudocode

---
+++ format_string_without_title.py
@@ -0,0 +1,5 @@
+# Allow the user to input a string
+# Use split() to separate each word
+# Use upper() and lower() on the first and remaining letters
+# Use join() to add all new words together
+# Print result
