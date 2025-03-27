11281fbbd22b3854af00ecab616ae5f0e595ba88
Jacuzzi <jheklayag@gmail.com>
2025-03-27 14:03:38 +0800

    Pseudocode

---
+++ format_string_without_swapcase.py
@@ -0,0 +1,4 @@
+# Allow the user to input a string
+# Use a loop and check for every uppercase and/or lowercase letter
+# Add or subtract 32 to every ASCII code of each uppercase and/or lowercase letter
+# Print result
