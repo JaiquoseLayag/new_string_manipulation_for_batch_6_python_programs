b6e101303c912198a756539604d14c4f17d4ffc9
Jacuzzi <jheklayag@gmail.com>
2025-03-27 13:53:15 +0800

    User input for string and desired length has been added

--- fortmat_string_without_center.py
+++ fortmat_string_without_center.py
@@ -1,4 +1,6 @@
 # Allow the user to input string and desired length
+string = input("Enter a string: ")
+desired_length = int(input("Enter your desired length: "))
 # Calculate how many spaces to be added to the left and right of the string
 # Manually add spaces to the left and right of the string
 # Print result
