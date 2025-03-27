204c99193e416c0f5b436ef5f6671d0030bc660e
Jacuzzi <jheklayag@gmail.com>
2025-03-27 13:54:46 +0800

    Spaces for left and right side of the string have been calculated

--- fortmat_string_without_center.py
+++ fortmat_string_without_center.py
@@ -2,5 +2,10 @@
 string = input("Enter a string: ")
 desired_length = int(input("Enter your desired length: "))
 # Calculate how many spaces to be added to the left and right of the string
+length = len(string)
+if length < desired_length:
+    total_spaces = desired_length - length
+    left_space = total_spaces // 2
+    right_space = total_spaces - left_space
 # Manually add spaces to the left and right of the string
 # Print result
