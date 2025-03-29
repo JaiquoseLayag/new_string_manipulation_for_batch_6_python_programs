e29a6929cc8722a11169693c572dd4eb0d61c0be
Jaiquose <jheklayag@gmail.com>
2025-03-29 20:19:13 +0800

    All lowercase letters has been checked

--- batch_7_format_string_without_upper.py
+++ batch_7_format_string_without_upper.py
@@ -1,5 +1,9 @@
 # Allow the user to input a string
 string = input("Enter a string: ")
 # Use a loop and check for every lowercase letter 
+lowercase_format = ""
+
+for characters in string:
+    if 'a' <= character <= 'z':
 # Subtract 32 to every ASCII code of each lowercase letter
 # Print result
