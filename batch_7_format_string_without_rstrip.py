b7373a7c371b840ee13a0901b150fc84aca2a679
Jaiquose <jheklayag@gmail.com>
2025-03-29 19:50:27 +0800

    User input for string has been added

--- batch_7_format_string_without_rstrip.py
+++ batch_7_format_string_without_rstrip.py
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 # Allow the user to input a string
 # Reverse iterate through each index for space characters
+=======
+# Allow the user to input a string
+string = input("Enter a string: ")
+# Reverse iterate through each index for space characters
+>>>>>>> 6a59d14 (User input for string has been added)
 # Print from the start of the index up to the end of a non-space character
