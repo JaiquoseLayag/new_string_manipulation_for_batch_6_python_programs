77b3af76cc9b0fb68d21fd975434d5397e9c9c7e
Jaiquose <jheklayag@gmail.com>
2025-03-29 19:52:22 +0800

    Checked for spaces in reverse index and removed mistakes

--- batch_7_format_string_without_rstrip.py
+++ batch_7_format_string_without_rstrip.py
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-# Allow the user to input a string
-# Reverse iterate through each index for space characters
-=======
 # Allow the user to input a string
 string = input("Enter a string: ")
 # Reverse iterate through each index for space characters
->>>>>>> 6a59d14 (User input for string has been added)
+index = len(string) - 1
+while index >= 0 and string[index] == " ":
+    index -= 1
+trimmed_text = string[:index + 1]
 # Print from the start of the index up to the end of a non-space character
