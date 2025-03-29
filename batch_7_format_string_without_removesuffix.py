cd5e93078bc4c52add53af69b1480216eb837470
Jaiquose <jheklayag@gmail.com>
2025-03-29 20:11:21 +0800

    User input for string and suffix has been added

--- batch_7_format_string_without_removesuffix.py
+++ batch_7_format_string_without_removesuffix.py
@@ -1,4 +1,6 @@
-# Allow the user to input a string
+# Allow the user to input a string and a suffix
+string = input("Enter a string: ")
+suffix = input("Enter the suffix used: ")
 # Use endswith() to check if the string ends with the suffix
 # Slice the string starting from the index without the suffix
 # Print result
