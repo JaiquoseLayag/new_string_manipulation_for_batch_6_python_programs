3a01b189c4e904cb9af05c518c2adaa3c0803373
Jaiquose <jheklayag@gmail.com>
2025-03-29 20:12:42 +0800

    String has been sliced up to the index before the suffix

--- batch_7_format_string_without_removesuffix.py
+++ batch_7_format_string_without_removesuffix.py
@@ -3,4 +3,8 @@ string = input("Enter a string: ")
 suffix = input("Enter the suffix used: ")
 # Use endswith() to check if the string ends with the suffix
 # Slice the string starting from the index without the suffix
+if string.endswith(suffix):
+    length = len(suffix)
+    
+    new_string = string[:len(string) - length] 
 # Print result
