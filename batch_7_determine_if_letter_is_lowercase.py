33407a2322e65daa61b8b4f2be13fa059584bd3a
Jaiquose <jheklayag@gmail.com>
2025-03-29 20:29:51 +0800

    Each character has been checked of the ASCII values and prints True when lowercase

--- batch_7_determine_if_letter_is_lowercase.py
+++ batch_7_determine_if_letter_is_lowercase.py
@@ -2,3 +2,9 @@
 string = input("Enter a string: ")
 # Use a loop and check the ASCII values of each letter
 # Return true if character is within a-z
+for character in string:
+    if 'A' <= character <= 'Z':
+        print("False")
+        break
+else:
+    print("True")
