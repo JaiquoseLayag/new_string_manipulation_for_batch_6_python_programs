b1d400f0021e6ea8a7724a2e7c5d4f20491c8410
Jaiquose <jheklayag@gmail.com>
2025-03-29 20:17:54 +0800

    User input for string has been added

--- batch_7_format_string_without_upper.py
+++ batch_7_format_string_without_upper.py
@@ -1,4 +1,5 @@
 # Allow the user to input a string
+string = input("Enter a string: ")
 # Use a loop and check for every lowercase letter 
 # Subtract 32 to every ASCII code of each lowercase letter
 # Print result
