3528b00c8b057df35b2c50dbfa36b56482252d2f
Jaiquose <jheklayag@gmail.com>
2025-03-29 20:21:28 +0800

    32 has been subtracted to the ASCII code and result has been printed

--- batch_7_format_string_without_upper.py
+++ batch_7_format_string_without_upper.py
@@ -1,9 +1,15 @@
 # Allow the user to input a string
 string = input("Enter a string: ")
 # Use a loop and check for every lowercase letter 
-lowercase_format = ""
+uppercase_format = ""
 
 for characters in string:
-    if 'a' <= character <= 'z':
+    if 'a' <= characters <= 'z':
 # Subtract 32 to every ASCII code of each lowercase letter
 # Print result
+
+        uppercase_format += chr(ord(characters) - 32)
+    else:
+        uppercase_format += characters
+
+print(uppercase_format)
