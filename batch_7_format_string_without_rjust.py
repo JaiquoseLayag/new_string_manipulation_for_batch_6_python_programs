886db1d51202ee0a09895fa7aab3738b4a573911
Jaiquose <jheklayag@gmail.com>
2025-03-30 18:07:39 +0800

    User input for string and desired length has been added

--- batch_7_format_string_without_rjust.py
+++ batch_7_format_string_without_rjust.py
@@ -1,3 +1,5 @@
 # Allow the user to input a string and the desired length
+string = input("Enter a string: ")
+desired_length = int(input("Enter the desired length: "))
 # Manually add spaces if the length of string is shorter than the desired length
 # Print result
