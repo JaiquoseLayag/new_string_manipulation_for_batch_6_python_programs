7e9028e97bebf3e93da48c8a36ddc4e812d7b36c
Jaiquose <jheklayag@gmail.com>
2025-03-30 18:08:26 +0800

    Spaces have been manually added to the left of the string

--- batch_7_format_string_without_rjust.py
+++ batch_7_format_string_without_rjust.py
@@ -2,4 +2,12 @@
 string = input("Enter a string: ")
 desired_length = int(input("Enter the desired length: "))
 # Manually add spaces if the length of string is shorter than the desired length
+length = len(string)
+
+if length < desired_length:
+    count = desired_length - length
+    count = " " * count
+    new_string = count + string
+else:
+    new_string = string
 # Print result
