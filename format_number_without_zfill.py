03d2e338c1e99cc16a9d09b27a4032372736f6aa
Jaiquose <jheklayag@gmail.com>
2025-03-30 18:20:24 +0800

    Zeroes are manually added before the number

--- format_number_without_zfill.py
+++ format_number_without_zfill.py
@@ -2,4 +2,12 @@
 number = int(input("Enter a number: "))
 desired_length = int(input("Enter the desired length: "))
 # Manually add zeroes before the number by taking its length
+length = len(str(number))
+
+if length < desired_length:
+    zeroes = "0" * (desired_length - length)
+    formatted_number = zeroes + str(number)
+else:
+    formatted_number = str(number)  
+
 # Print result
