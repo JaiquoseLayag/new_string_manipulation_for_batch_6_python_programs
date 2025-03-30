d17a18ec2be2d3fc50ca88c9a4f1ea00608ce269
Jaiquose <jheklayag@gmail.com>
2025-03-30 18:34:05 +0800

    Loops through the string and checked if it matches with the substring

--- batch_7_count_string_without_count_func.py
+++ batch_7_count_string_without_count_func.py
@@ -4,7 +4,16 @@ main_string = input("Enter the main string: ")
 substring = input("Enter the substring to search for: ")
 
 count = 0
-# Get the lengths of both strings
+
+main_length = len(main_string)
+sub_length = len(substring)
 # Loop through the main string and check for matches
 # Check if it matches the search substring
+for i in range(main_length - sub_length + 1):  
+    current_part = main_string[i:i + sub_length]
+    
+    
+    if current_part == substring:
+        count += 1  
+        
 # Print result
