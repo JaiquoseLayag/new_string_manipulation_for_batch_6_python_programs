0656e611f7be8c82add4ab8fa09cd432468ab3e8
Jaiquose <jheklayag@gmail.com>
2025-03-30 18:33:13 +0800

    User input for string has been added and initialized a count

--- batch_7_count_string_without_count_func.py
+++ batch_7_count_string_without_count_func.py
@@ -1,5 +1,9 @@
 # Get input from the user
 # Initialize a count
+main_string = input("Enter the main string: ")
+substring = input("Enter the substring to search for: ")
+
+count = 0
 # Get the lengths of both strings
 # Loop through the main string and check for matches
 # Check if it matches the search substring
