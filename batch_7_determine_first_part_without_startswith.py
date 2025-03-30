940b5062d2e57ac5a1112f6840b39def46a2b7b6
Jaiquose <jheklayag@gmail.com>
2025-03-30 17:56:43 +0800

    User input for string and first part has been added

--- batch_7_determine_first_part_without_startswith.py
+++ batch_7_determine_first_part_without_startswith.py
@@ -1,4 +1,6 @@
 # Allow the user to input a string and the first part of the string
+string = input("Enter a string: ")
+first_part = input("Enter the first part: ")
 # Slice the string up to the first part
 # Compare with the length of the first part
 # Print result
