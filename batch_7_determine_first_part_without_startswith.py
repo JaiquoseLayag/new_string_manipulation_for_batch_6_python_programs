f35250c854fb122d2c1ea1d7a429b5ca3e80bd4f
Jaiquose <jheklayag@gmail.com>
2025-03-30 17:58:09 +0800

    First part of the string is compared to the length of the substring

--- batch_7_determine_first_part_without_startswith.py
+++ batch_7_determine_first_part_without_startswith.py
@@ -3,4 +3,7 @@ string = input("Enter a string: ")
 first_part = input("Enter the first part: ")
 # Slice the string up to the first part
 # Compare with the length of the first part
+length = len(first_part)
+
+if string[:length] == first_part:
 # Print result
