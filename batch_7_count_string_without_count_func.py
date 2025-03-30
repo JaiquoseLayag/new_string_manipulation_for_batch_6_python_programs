7b7d5756e548211e8aa669fd6519a89092c11fe7
Jaiquose <jheklayag@gmail.com>
2025-03-30 18:32:33 +0800

    Pseudocode

---
+++ batch_7_count_string_without_count_func.py
@@ -0,0 +1,6 @@
+# Get input from the user
+# Initialize a count
+# Get the lengths of both strings
+# Loop through the main string and check for matches
+# Check if it matches the search substring
+# Print result
