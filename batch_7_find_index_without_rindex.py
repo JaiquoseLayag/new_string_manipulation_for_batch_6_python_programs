41259c4e4a7c2b9f9dd9edb2dea6a5baf2ce04c9
Jaiquose <jheklayag@gmail.com>
2025-03-30 22:17:44 +0800

    Reverse loops through the string and finds index

--- batch_7_find_index_without_rindex.py
+++ batch_7_find_index_without_rindex.py
@@ -2,4 +2,13 @@
 text = input("Enter the main string: ")
 substring = input("Enter the substring to find: ")
 # Loop through the string in reverse
+text_length = len(text)
+sub_length = len(substring)
+
+found_index = -1
+
+for i in range(text_length - sub_length, -1, -1):
+    if text[i:i + sub_length] == substring:
+        found_index = i
+        break
 # Print result
