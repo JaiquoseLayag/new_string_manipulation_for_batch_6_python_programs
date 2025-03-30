a44c7b5bf9ed25e9053f243ae20e34e060998047
Jaiquose <jheklayag@gmail.com>
2025-03-30 18:19:54 +0800

    User input for number and desired length has been added

--- format_number_without_zfill.py
+++ format_number_without_zfill.py
@@ -1,3 +1,5 @@
 # Allow the user to input a number and desired length
+number = int(input("Enter a number: "))
+desired_length = int(input("Enter the desired length: "))
 # Manually add zeroes before the number by taking its length
 # Print result
